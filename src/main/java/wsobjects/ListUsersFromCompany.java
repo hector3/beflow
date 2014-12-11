629e558f1725b5d657bc9c0e35af416df9534bae
Lidia <karrucha.1986@gmail.com>
2014-12-11 19:34:02 +0100

    tots crud ok + API Ok(ja retorna llista de users y llistat de companyies)

--- src/main/java/wsobjects/ListUsersFromCompany.java
+++ src/main/java/wsobjects/ListUsersFromCompany.java
@@ -3,6 +3,9 @@ package wsobjects;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.glassfish.jersey.archetypes.jersey.quickstart.webapp.Company;
+import org.glassfish.jersey.archetypes.jersey.quickstart.webapp.User;
+
 public class ListUsersFromCompany {
 	public List <UserWS> listaUsersws;
 	
@@ -13,4 +16,29 @@ public class ListUsersFromCompany {
 	public void add (UserWS user){
 		listaUsersws.add(user);
 	}
+	
+	
+	 public ArrayList<UserWS> getList(List<User> lista) {
+			
+			List <UserWS> listaws=new ArrayList<UserWS>();
+			
+			try{
+				
+				
+				for (User tempUser : lista) {
+					
+					UserWS user = new UserWS(0,tempUser.getLogin(),tempUser.getPassword(),tempUser.getRole(),tempUser.getName(),tempUser.getPhone(),tempUser.getDepartment(),tempUser.getCompany().getCompany_name());
+					listaws.add(user);
+					System.out.println("Usuario: "+user.getLogin());
+					
+					}
+			}
+			
+			
+			catch(Exception e){
+				
+				System.out.println("problemas.. Excepcion: "+e.getMessage());
+			}
+			return (ArrayList<UserWS>) listaws;
+	 }
 }
